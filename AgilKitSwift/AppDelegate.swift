69eaa892fc991549eecaab5e1d3489043693cd44
Shane Meyer <shane@g.com>
2016-11-14 07:05:27 -0800

    Updates for Xcode 8.1.

--- AgilKitSwift/AppDelegate.swift
+++ AgilKitSwift/AppDelegate.swift
@@ -27,8 +27,8 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-	func application(_ application: UIApplication,
-		didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions
+		launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil) -> Bool
 	{
 		return true
 	}
